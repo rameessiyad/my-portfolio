3827c9249a3b692b16d9c911c48bf61c8a68a312
rameessiyad <rameesmsiyadofficial786@gmail.com>
2026-05-22 07:12:59 +0530

    feat:navbar, footer and hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto">
+          <main className="flex-1">{children}</main>
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
